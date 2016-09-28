7d939adcaabd83a79fb39e6b60382b2df4f2d10e
ChristianFML <mor15015@uvg.edu.gt>
2016-09-27 20:12:10 -0600

    Add files via upload

---
+++ Association.java
@@ -0,0 +1,83 @@
+import java.util.Map;
+import java.io.*;
+
+
+public class Association<K,V> implements Map.Entry<K,V>
+{
+
+    protected K tKey; 
+    protected V tValor; 
+    
+    public Association(K key, V valor)
+    {
+        tKey = key;
+        tValor = valor;
+    }
+
+ 
+    public Association(K key)
+    {
+        this(key,null);
+    }
+
+
+    public boolean equals(Object other)
+    {
+        Association otherAssoc = (Association)other;
+        return getKey().equals(otherAssoc.getKey());
+    }
+    
+
+    public int hashCode()
+    {
+        return getKey().hashCode();
+    }
+    
+
+    public V getValue()
+    {
+        return tValor;
+    }
+
+
+    public K getKey()
+    {
+        return tKey;
+    }
+
+
+    public V setValue(V value)
+    {
+        V oldValue = tValor;
+        tValor = value;
+        return oldValue;
+    }
+
+
+    public String toString()
+    {
+        StringBuffer s = new StringBuffer();
+        s.append("<Association: "+getKey()+"="+getValue()+">");
+        return s.toString();
+    }
+    
+    public String leerArchivo(String direccion){
+
+    		String resultado = "";
+    		try {
+    			FileReader f = new FileReader(direccion);
+    			BufferedReader b = new BufferedReader(f);
+    			try {
+    				resultado = b.readLine();
+    				b.close();
+    			} catch (IOException e) {
+    				return "No se puede abrir archivo";
+    			}
+    		} catch (FileNotFoundException e) {
+    			return "Archivo no encontrado";
+    		}
+    		return resultado;
+    	}
+    
+
+}
---
+++ BinaryTree.java
@@ -0,0 +1,108 @@
+public class BinaryTree<E>{
+	
+protected E val;
+protected BinaryTree<E> parent,left,right;
+
+
+	public BinaryTree() {
+		// Se genera un nodo vacio.
+		
+		val = null;
+		parent = null;
+		left=right=this;
+	}
+	
+	public BinaryTree(E valor) {
+		// TODO Auto-generated constructor stub
+		
+		val = valor;
+		right=left= new BinaryTree<E>();
+		setLeft(left);
+		setRight(right);
+		
+	}
+
+	public BinaryTree(E valor, BinaryTree<E> left, BinaryTree<E> right) {
+		// TODO Auto-generated constructor stub
+		val=valor;
+		if(left==null){
+			left = new BinaryTree<E>();
+		}
+		
+		setLeft(left);
+		
+		if(right == null){
+			right = new BinaryTree<E>();
+		}
+		
+		setRight(right);
+		
+	}
+	
+	public BinaryTree<E> left(){
+		
+		return left;
+		
+	}
+	
+	public BinaryTree<E> parent(){
+		
+		return parent;
+	}
+	
+	public BinaryTree<E> right(){
+		
+		return right;
+		
+	}
+	
+	public void setLeft(BinaryTree<E> newLeft){
+		
+		if(isEmpty()){
+			return;
+		}
+		if (left != null && left.parent() == this){
+			left.setParent(null);
+			left = newLeft;
+			left.setParent(this);
+		}
+	}
+	
+	public void setRight(BinaryTree<E> newRight){
+		
+		if(isEmpty()){
+			return;
+		}
+		if (right != null && right.parent() == this){
+			right.setParent(null);
+			right = newRight;
+			right.setParent(this);
+		}
+	}
+	
+	public void setParent(BinaryTree<E> newParent){
+		if (!isEmpty()){
+			parent=newParent;
+		}
+	}
+	
+	
+	public E value(){
+		return val;
+		
+	}
+	public void setValue(E valor){
+		val=valor;
+	}
+
+	public boolean isEmpty(){
+		boolean empty=false;
+		if(parent==null)
+			empty=true;
+		else
+			return empty;
+		return empty;
+		
+	}
+
+}
